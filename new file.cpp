28d54a9fa21905e6c273764898d70c2fb980e88d
AndrewRayner63 <120320330+AndrewRayner63@users.noreply.github.com>
2024-10-26 17:45:40 +0000

    owzhat commit

--- new file.cpp
+++ new file.cpp
@@ -1,4 +1,32 @@
+
 #include <iostream>
-int main(){
+#include <vector>
+
+int main ()
+{
+    std::cout<<"hello world!\n";
+ std::vector <std::string> players;
+ std::string string;
+int batcount{0};
+while(batcount<5){
+if (batcount==0)
+{  
+    std::cout<<"opening batsman name ";
+    std::cin>> string;
+    players.push_back (string);
+    
+    std::cout<<"next batsman name ";
+    std::cin>> string;
+    players.push_back (string);
+    batcount=2;
+}else{
+    std::cout<<"New batsman name ";
+    std::cin>> string;
+    players.push_back (string);
+    batcount++;
+}}
+std::cout<<players[players.size()-1] << std::endl;
+std::cout<<players.size();
+
     return 0;
 }
